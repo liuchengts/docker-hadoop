463c277422f8b3a21299a8ce4445ac52685a37ee
刘成 <790809568@qq.com>
2019-06-25 20:46:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# docker-hadoop
+## 感谢 https://github.com/kiwenlau/hadoop-cluster-docker 出第一版的源代码，本项目是在 hadoop-cluster-docker的基础上进行了更新
+* 更新hadoop版本为3.1.2 
+* jdk改用 oracle jdk1.8.0_201
+* 增加了3.x版本的兼容配置
+
+
+
---
+++ resize-cluster.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# N is the node number of hadoop cluster
+N=$1
+
+if [[ $# = 0 ]]
+then
+	echo "Please specify the node number of hadoop cluster!"
+	exit 1
+fi
+
+# change slaves file
+i=1
+rm docker-config/workers
+while [[ ${i} -lt ${N} ]]
+do
+	echo "hadoop-slave$i" >> docker-config/workers
+	((i++))
+done 
+
+echo ""
+
+echo -e "\nbuild docker hadoop image\n"
+
+# rebuild kiwenlau/hadoop image
+sudo docker build -t lc/hadoop:1.0 .
+
+echo ""
